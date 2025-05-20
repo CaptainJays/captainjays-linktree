231e31a47e7d7dc1c10dfaedc103191b3db67ee1
Ali Sayed 1 <alis@captainj.dom>
2025-05-20 15:22:46 -0400

    added insta and tiktok

--- components/links.tsx
+++ components/links.tsx
@@ -2,7 +2,7 @@
 
 import { motion } from "framer-motion";
 import { Clock4, CircleDollarSign, Globe, File, FileUser, Mail,  ScrollText, ListChecks } from "lucide-react";
-import { FaBehance,FaDribbble, FaTruckMoving} from "react-icons/fa";
+import { FaBehance,FaDribbble, FaInstagram, FaTiktok, FaTruckMoving} from "react-icons/fa";
 import { SiRoadmapdotsh, SiDoordash, SiUbereats } from "react-icons/si";
 import { PiExamBold } from "react-icons/pi";
 import { TbBrandPepsi } from "react-icons/tb";
@@ -13,48 +13,109 @@ interface LinksProps {
 }
 
 const links = [
-  {
-    title: "Hr Documents",
-    url: "/hrdoc",
-    icon: File,
-    style: "outline" //gradient
-  },
-  {
-    title: "Road Maps",
-    url: "/roadmaps",
-    icon: SiRoadmapdotsh,
-    style: "outline"
-  },
-  {
-    title: "Captain Jays Exams",
-    url: "https://app.jotform.com/250494972265163",
-    icon: PiExamBold,
-    style: "outline"
-  },
-  {
-    title: "Time Clock",
-    url: "https://www.swipeclock.com/pg/Login.aspx?why=notloggedin&",
-    icon: Clock4,
-    style: "outline"
-  },
-  {
-    title: "DFD Truck Order",
-    url: "https://app.jotform.com/250266173293154",
-    icon: FaTruckMoving,
-    style: "outline"
-  },
-  {
-    title: "Pepsi",
-    url: "https://secure.pepsico.com/oauth2/aus1ljxpv3z9G8NGl0h8/v1/authorize?response_type=code&scope=openid%20email&client_id=0oa1ljamd68SiMPyP0h8&state=EXj_qWOZIze1P6rAgI3LSrieS0w&redirect_uri=https%3A%2F%2Fwww.mypepsico.com%3A443%2Fep&nonce=ouuOru4gE3keutR8SM1iYyit3JlLMVx2wH8LEWjSl9Y",
-    icon: TbBrandPepsi,
-    style: "outline"
-  },
-  {
-    title: "Daily Checklists",
-    url: "/dailychecklists",
-    icon: ListChecks,
-    style: "outline"
-  },
+  // {
+  //   title: "Hr Documents",
+  //   url: "/hrdoc",
+  //   icon: File,
+  //   style: "outline" //gradient
+  // },
+  // {
+  //   title: "Road Maps",
+  //   url: "/roadmaps",
+  //   icon: SiRoadmapdotsh,
+  //   style: "outline"
+  // },
+  // {
+  //   title: "Captain Jays Exams",
+  //   url: "https://app.jotform.com/250494972265163",
+  //   icon: PiExamBold,
+  //   style: "outline"
+  // },
+  // {
+  //   title: "Time Clock",
+  //   url: "https://www.swipeclock.com/pg/Login.aspx?why=notloggedin&",
+  //   icon: Clock4,
+  //   style: "outline"
+  // },
+  // {
+  //   title: "DFD Truck Order",
+  //   url: "https://app.jotform.com/250266173293154",
+  //   icon: FaTruckMoving,
+  //   style: "outline"
+  // },
+  // {
+  //   title: "Pepsi",
+  //   url: "https://secure.pepsico.com/oauth2/aus1ljxpv3z9G8NGl0h8/v1/authorize?response_type=code&scope=openid%20email&client_id=0oa1ljamd68SiMPyP0h8&state=EXj_qWOZIze1P6rAgI3LSrieS0w&redirect_uri=https%3A%2F%2Fwww.mypepsico.com%3A443%2Fep&nonce=ouuOru4gE3keutR8SM1iYyit3JlLMVx2wH8LEWjSl9Y",
+  //   icon: TbBrandPepsi,
+  //   style: "outline"
+  // },
+  // {
+  //   title: "Daily Checklists",
+  //   url: "/dailychecklists",
+  //   icon: ListChecks,
+  //   style: "outline"
+  // },
+  // {
+  //   title: "Captain Jay's Website",
+  //   url: "https://www.captainjays.net/",
+  //   icon: Globe,
+  //   style: "outline"
+  // },
+  // {
+  //   title: "Doordash",
+  //   url: "https://identity.doordash.com/auth?client_id=1643580605860775164&redirect_uri=https%3A%2F%2Fmerchant-portal.doordash.com%2Fauth_callback&scope=*&prompt=none&response_type=code&layout=merchant_web_v2&state=abd230e6-1bfb-49c2-8bde-6a5f95e8592a&allowRedirect=true&failureRedirect=%2Fmerchant%2Flogin",
+  //   icon: SiDoordash,
+  //   style: "outline"
+  // },
+  // {
+  //   title: "Uber Eats",
+  //   url: "https://merchants.ubereats.com/manager/home",
+  //   icon: SiUbereats,
+  //   style: "outline"
+  // },
+  // {
+  //   title: "Venture (Onboarding)",
+  //   url: "https://vns.prismhr.com/vns/",
+  //   icon: CircleDollarSign,
+  //   style: "outline"
+  // },
+  // {
+  //   title: "Team Member Applications",
+  //   url: "https://mega.nz/file/bFgh2RYR#rvz62tgT_b0X4dhIJoP8jBj1Z4kmM_T9dv1CZ_WYaQ4",
+  //   icon: FileUser,
+  //   style: "outline"
+  // },
+  // {
+  //   title: "Employee Guidelines",
+  //   url: "https://mega.nz/file/SQYGHLzD#ZQR7CPr4Ouj-r_L2PfxdpFsgxpj5Scejl5l5l0aSGg8",
+  //   icon: ScrollText,
+  //   style: "outline"
+  // },
+  // {
+  //   title: "Benefits Enrollment Form",
+  //   url: "https://mega.nz/file/3UwS1QIC#BjCdHHTWXFxQbT6QFn6d4AtFA4DesszsBSR41IaR1sE",
+  //   icon: ScrollText,
+  //   style: "outline"
+  // },
+  // {
+  //   title: "Customer Incident Report",
+  //   url: "https://mega.nz/file/LYJBwa7Z#22YWRd5HT54of4bbHNTbasJ3MnyGzi-8OVKAG4PmAz0",
+  //   icon: ScrollText,
+  //   style: "outline"
+  // },
+  // {
+  //   title: "Food Cost Percentage Budget",
+  //   url: "https://mega.nz/file/eRA3XR4K#EA9ssGPOr9F0szHehg_tN7CJHLTgpe24RFCdHfR3Yww",
+  //   icon: ScrollText,
+  //   style: "outline"
+  // },
+  // {
+  //   title: "Employee Incident Report",
+  //   url: "https://mega.nz/file/PY5T3LqY#tUSyGwmgMb144vzC1wDCIcC14QWXX2MYQ-qEsuKvAgo", 
+  //   icon: ScrollText,
+  //   style: "outline"
+  // }
+  
   {
     title: "Captain Jay's Website",
     url: "https://www.captainjays.net/",
@@ -62,59 +123,17 @@ const links = [
     style: "outline"
   },
   {
-    title: "Doordash",
-    url: "https://identity.doordash.com/auth?client_id=1643580605860775164&redirect_uri=https%3A%2F%2Fmerchant-portal.doordash.com%2Fauth_callback&scope=*&prompt=none&response_type=code&layout=merchant_web_v2&state=abd230e6-1bfb-49c2-8bde-6a5f95e8592a&allowRedirect=true&failureRedirect=%2Fmerchant%2Flogin",
-    icon: SiDoordash,
+    title: "Captain Jay's Instagram",
+    url: "https://www.instagram.com/captainjays/?hl=en",
+    icon: FaInstagram,
     style: "outline"
   },
   {
-    title: "Uber Eats",
-    url: "https://merchants.ubereats.com/manager/home",
-    icon: SiUbereats,
+    title: "Captain Jay's TikTok",
+    url: "https://www.tiktok.com/@captain.jays",
+    icon: FaTiktok,
     style: "outline"
   },
-  {
-    title: "Venture (Onboarding)",
-    url: "https://vns.prismhr.com/vns/",
-    icon: CircleDollarSign,
-    style: "outline"
-  },
-  {
-    title: "Team Member Applications",
-    url: "https://mega.nz/file/bFgh2RYR#rvz62tgT_b0X4dhIJoP8jBj1Z4kmM_T9dv1CZ_WYaQ4",
-    icon: FileUser,
-    style: "outline"
-  },
-  {
-    title: "Employee Guidelines",
-    url: "https://mega.nz/file/SQYGHLzD#ZQR7CPr4Ouj-r_L2PfxdpFsgxpj5Scejl5l5l0aSGg8",
-    icon: ScrollText,
-    style: "outline"
-  },
-  {
-    title: "Benefits Enrollment Form",
-    url: "https://mega.nz/file/3UwS1QIC#BjCdHHTWXFxQbT6QFn6d4AtFA4DesszsBSR41IaR1sE",
-    icon: ScrollText,
-    style: "outline"
-  },
-  {
-    title: "Customer Incident Report",
-    url: "https://mega.nz/file/LYJBwa7Z#22YWRd5HT54of4bbHNTbasJ3MnyGzi-8OVKAG4PmAz0",
-    icon: ScrollText,
-    style: "outline"
-  },
-  {
-    title: "Food Cost Percentage Budget",
-    url: "https://mega.nz/file/eRA3XR4K#EA9ssGPOr9F0szHehg_tN7CJHLTgpe24RFCdHfR3Yww",
-    icon: ScrollText,
-    style: "outline"
-  },
-  {
-    title: "Employee Incident Report",
-    url: "https://mega.nz/file/PY5T3LqY#tUSyGwmgMb144vzC1wDCIcC14QWXX2MYQ-qEsuKvAgo", 
-    icon: ScrollText,
-    style: "outline"
-  }
 ];
 
 const container = {
